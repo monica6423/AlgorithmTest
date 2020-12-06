9c87c3a597edc8335c617d345b8027ab292d65f1
monica6423 <monica6423@gmail.com>
2020-12-06 19:15:04 +0800

    capitalize sol

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,30 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for(let i = 1; i < str.length; i++) {
+      //to the left of the current character is a space, then take current character to uppercase
+      //add that on to the result
+      if(str[i - 1] === ' ') {
+          result += str[i].toUpperCase();
+      }else {
+          result += str[i];
+      }
+  }
+  
+  return result;
+}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//     const words = [];
+
+//     for(let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+
+//     return words.join(' ');
+// }
