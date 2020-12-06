11e78304e0568d713a783078ec1891f3de539c90
monica6423 <monica6423@gmail.com>
2020-12-06 14:40:33 +0800

    anagram sol2

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,7 +9,11 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-    
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
